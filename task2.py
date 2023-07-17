8430c2b2101e29e2916b6d60ebcb3946f1ef11df
unknown <smtishkar@mail.ru>
2023-07-17 22:58:30 +0300

    добавил константы

--- task2.py
+++ task2.py
@@ -10,41 +10,51 @@
 
 sum = 0
 count = 0
+RICH = 5_000_000
+WEALTH_TAX = 0.1
+ADDITIONAL_RATE = 0.03
+MIN_COMMISION = 30
+MAX_COMMISION = 600
+COMMISION = 0.015
+AMOUNT_LIMIT = 50
+ADDITIONAL_RATE_PERIOD = 3
 
 while True:
     command = input("введите действие: ")
     if command == "+":
         count += 1
         money = int(input("Введите сумму: "))
-        if money % 50 == 0 and sum < 5_000_000:
+        if money % AMOUNT_LIMIT == 0 and sum < RICH:
             sum += money
             print(sum)
-        elif money % 50 == 0 and sum > 5_000_000:
-            sum =  (sum + money) - ((sum + money) * 0.1)          
+        elif money % AMOUNT_LIMIT == 0 and sum > RICH:
+            sum =  (sum + money) - ((sum + money) * WEALTH_TAX)          
             print(sum)
         else:
             print("сумма введена некорретно")
-        if count % 3 == 0:
-            sum -= sum * 0.03
-            print("комиссия - ", sum * 0.03)
+        if count % ADDITIONAL_RATE_PERIOD == 0:
+            sum -= sum * ADDITIONAL_RATE
+            print("Будет списанна комиссия - ", sum * ADDITIONAL_RATE)     #Сделать чтобы начилсяслись процентыб а не снимались
+            print(sum)
     if command == "-" and sum > 50:
         count += 1
         money = int(input("Введите сумму: "))
-        if money % 50 == 0:
-            if money * 0.015 < 30:
-                sum -= (money + 30)
+        if money % AMOUNT_LIMIT == 0:
+            if money * COMMISION < MIN_COMMISION:
+                sum -= (money + MIN_COMMISION)
 
-            elif money * 0.015 > 600:
-                sum -= (money + 600)
+            elif money * COMMISION > MAX_COMMISION:
+                sum -= (money + MAX_COMMISION)
 
             else:
-                sum -= (money + (money * 0.015))
+                sum -= (money + (money * COMMISION))
             print(sum)
         else:
             print("сумма введена некорретно")
-            if count % 3 == 0:
-                sum -= sum * 0.03
-                print("комиссия - ", sum * 0.03)
+            if count % ADDITIONAL_RATE_PERIOD == 0:
+                sum -= sum * ADDITIONAL_RATE
+                print("Будет списана комиссия - ", sum * ADDITIONAL_RATE)          #Сделать чтобы начилсяслись проценты, а не снимались
+                print(sum)
     if command == "e":
         print(sum)
         break

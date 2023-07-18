e3e01c8dcc44daf9f9fe5b7151870b9903cc6a10
unknown <smtishkar@mail.ru>
2023-07-18 16:14:08 +0300

    Задача решена без методов

--- task2.py
+++ task2.py
@@ -9,6 +9,7 @@
 # ✔ Любое действие выводит сумму денег
 
 sum = 0
+temp = 0
 count = 0
 RICH = 5_000_000
 WEALTH_TAX = 0.1
@@ -17,7 +18,10 @@ MIN_COMMISION = 30
 MAX_COMMISION = 600
 COMMISION = 0.015
 AMOUNT_LIMIT = 50
+START_MINIMUM_LIMIT = 50
 ADDITIONAL_RATE_PERIOD = 3
+
+
 
 while True:
     command = input("введите действие: ")
@@ -32,31 +36,73 @@ while True:
             print("Ваш баланс - ", sum)
         else:
             print("сумма введена некорретно")
+
         if count % ADDITIONAL_RATE_PERIOD == 0:
             sum += sum * ADDITIONAL_RATE
             print("Вам дополнительный доход - ", sum * ADDITIONAL_RATE)     #подумать нужно ли тут округление?
             print("Ваш баланс - ", sum)
-    if command == "-" and sum > 50:                 # Уходим в минус и снимается еще деньги за операцию
+    if command == "-" and sum >= START_MINIMUM_LIMIT:                
         count += 1
         money = int(input("Введите сумму: "))
-        if money % AMOUNT_LIMIT == 0 and sum >= money :
-            if money * COMMISION < MIN_COMMISION:
-                sum -= (money + MIN_COMMISION)
-
-            elif money * COMMISION > MAX_COMMISION:
-                sum -= (money + MAX_COMMISION)
-
-            else:
-                sum -= (money + (money * COMMISION))
-            print("Ваш баланс - ", sum)
-        elif money % AMOUNT_LIMIT == 0 and sum < money :
-            print("Недостаточно средств")
-        else:
-            print("сумма введена некорретно")
-            if count % ADDITIONAL_RATE_PERIOD == 0:
-                sum += sum * ADDITIONAL_RATE
-                print("Вам дополнительный доход - ", sum * ADDITIONAL_RATE)          #Сделать чтобы начилсяслись проценты, а не снимались
+        if sum < RICH:
+            if money % AMOUNT_LIMIT == 0 and sum >= money:
+                if money * COMMISION < MIN_COMMISION:
+                    temp = sum - (money + MIN_COMMISION)
+                    if temp > 0:
+                        sum -= (money + MIN_COMMISION)
+                    else: print ("Операция не возможна")
+                    temp = 0
+                elif money * COMMISION > MAX_COMMISION:
+                    temp = sum - (money + MAX_COMMISION)
+                    if temp > 0:
+                        sum -= (money + MAX_COMMISION)
+                    else: print ("Операция не возможна")
+                    temp = 0 
+                    # sum -= (money + MAX_COMMISION)
+                else:
+                    temp = sum - (money * COMMISION)
+                    if temp > 0:
+                        sum -= (money * COMMISION)
+                    else: print ("Операция не возможна")
+                    temp = 0 
+                    # sum -= (money + (money * COMMISION))
                 print("Ваш баланс - ", sum)
+            elif money % AMOUNT_LIMIT == 0 and sum < money :
+                print("Недостаточно средств")
+            else:
+                print("сумма введена некорретно")
+                if count % ADDITIONAL_RATE_PERIOD == 0:
+                    sum += sum * ADDITIONAL_RATE
+                    print("Вам дополнительный доход - ", sum * ADDITIONAL_RATE) 
+                    print("Ваш баланс - ", sum)
+        else:
+            if money % AMOUNT_LIMIT == 0 and sum >= money:
+                if money * COMMISION < MIN_COMMISION:             
+                    sum -= (money + MIN_COMMISION) 
+                    sum -= sum * WEALTH_TAX
+                elif money * COMMISION > MAX_COMMISION:
+                    temp = sum - ((money + MAX_COMMISION) + sum * WEALTH_TAX)
+                    if temp > 0:
+                        sum -= ((money + MAX_COMMISION) + sum * WEALTH_TAX)
+                    else: print ("Операция не возможна")
+                    temp = 0       
+                    # sum -= (money + MAX_COMMISION)
+                    # sum -= sum * WEALTH_TAX
+                else:
+                    sum -= (money + (money * COMMISION))
+                    sum -= sum * WEALTH_TAX
+                print("Ваш баланс - ", sum)
+            elif money % AMOUNT_LIMIT == 0 and sum < money :
+                print("Недостаточно средств")
+            else:
+                print("сумма введена некорретно")
+                if count % ADDITIONAL_RATE_PERIOD == 0:
+                    sum += sum * ADDITIONAL_RATE
+                    print("Вам дополнительный доход - ", sum * ADDITIONAL_RATE) 
+                    print("Ваш баланс - ", sum)
+
+    if command == "-" and sum < START_MINIMUM_LIMIT: 
+        print ("На счету не достаточно средств для снятия")
     if command == "e":
         print("Ваш баланс - ", sum)
         break
